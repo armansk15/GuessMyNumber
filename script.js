538077882e53cfd9f30d3f2a9f53e1429fe8ee58
armansk15 <no-reply>
2022-06-06 20:34:44 +0300

    add some explantion

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+
+    //When player wins
   } else if (guess === randomNumber) {
     document.querySelector('.message').textContent = '🥳 Correct number!';
+
+    // When guess is too high
   } else if (guess > randomNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -20,6 +25,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '🤷‍♂️ You lost!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // When guess is too low
   } else if (guess < randomNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low!';
