b7cbb52329033d5459db156c768bca259d2c993e
armansk15 <no-reply>
2022-06-06 20:04:48 +0300

    fixed a bug

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-let randomNumber = Math.trunc(Math.random() * 21);
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = randomNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -9,5 +10,9 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '⛔ No number!';
   } else if (guess === randomNumber) {
     document.querySelector('.message').textContent = '🥳 Correct number!';
+  } else if (guess > randomNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+  } else if (guess < randomNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
   }
 });
