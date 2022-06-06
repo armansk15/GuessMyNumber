7cb1b1f2b7ad708c3dd0191559b633c635cfe656
armansk15 <no-reply>
2022-06-06 20:15:46 +0300

    score decrease after wrong input

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = randomNumber;
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -12,7 +13,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🥳 Correct number!';
   } else if (guess > randomNumber) {
     document.querySelector('.message').textContent = '📈 Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
   } else if (guess < randomNumber) {
     document.querySelector('.message').textContent = '📉 Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
