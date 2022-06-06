1f2593c946114dc0d1b81358c081da2d37ce351e
armansk15 <no-reply>
2022-06-06 18:30:57 +0300

    add first lines of code

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let randomNumber = Math.trunc(Math.random() * 21);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === randomNumber) {
+    document.querySelector('.message').textContent = '🥳 Correct number!';
+  }
+});
